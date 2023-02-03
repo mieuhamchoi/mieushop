5c228f43f533e8a62d2413f05a89c9d7067b8c51
Mieu <mieuhamchoi@gmail.com>
2023-02-03 20:15:36 +0700

    add anglular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+// routing
 import { AppRoutingModule } from './app-routing.module';
+// compoent app
 import { AppComponent } from './app.component';
+
+// component create
 import { HeaderComponent } from './components/shared/components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 import { PageNotFoundComponent } from './components/shared/components/page-not-found/page-not-found.component';
+
+//angular material
+import {MatInputModule} from '@angular/material/input'
+import {MatAutocompleteModule} from '@angular/material/autocomplete'
 
 @NgModule({
   declarations: [
@@ -16,7 +24,10 @@ import { PageNotFoundComponent } from './components/shared/components/page-not-f
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/shared/components/header/header.component.ts
+++ src/app/components/shared/components/header/header.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  public options = ['Bạn 1', 'Bạn 2', 'Bạn 3']
 
 }

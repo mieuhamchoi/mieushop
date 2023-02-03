b29a680bbdd3cf54911ab7809076047b1beef563
Mieu <mieuhamchoi@gmail.com>
2023-02-03 15:53:09 +0700

    build project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { PageNotFoundComponent } from './components/shared/components/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,

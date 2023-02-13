69bd03020b60e1587486e049e321180110123609
Mieu <mieuhamchoi@gmail.com>
2023-02-13 18:10:15 +0700

    comment location use pipe

--- src/app/components/shared/pipes/formatVnd/format-vnd.pipe.ts
+++ src/app/components/shared/pipes/formatVnd/format-vnd.pipe.ts
@@ -4,7 +4,7 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'formatVnd'
 })
 export class FormatVndPipe implements PipeTransform {
-
+  // customer pipe
   transform(value: number, ...args: number[]): string {
     let decimalPlaces = value.toString().length; // Để xác định số chữ số của một số thực
     let formattedNumber = value.toLocaleString('vi-VN', { style: 'decimal', maximumFractionDigits: decimalPlaces });
